e08e1486593b10f3f7fc9275bc0e45eb3dee4a38
Fenix <andrarias@gmail.com>
2019-06-28 15:37:35 -0300

    deNuevo DB

--- Server/build/keys.js
+++ Server/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'aa1v5oaurv6oyc5.cjhnbwblq7zm.us-east-2.rds.amazonaws.com',
-        user: 'BarracaCardona',
-        password: 'BarracaCardona',
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'barracadb' //ebdb // 'barracadb'   //  'ng_prueba_db
     }
 };
