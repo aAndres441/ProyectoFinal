414e6234db2275c1a2518c5972cf590e1daa8b3a
Fenix <andrarias@gmail.com>
2019-07-02 09:57:42 -0300

    baseCfreada

--- Server/SRC/keys.ts
+++ Server/SRC/keys.ts
@@ -1,8 +1,9 @@
 export default{
     database: {
-        host: 'localhost',  //'localhost',  //aa1v5oaurv6oyc5.cjhnbwblq7zm.us-east-2.rds.amazonaws.com
-        user: 'root',  //'root',BarracaCardona
-        password: '', //'',  // BarracaCardona
-        database:  'barracadb'  //ebdb // 'barracadb'   //  'ng_prueba_db
+        host: 'barracabasedatos.cjhnbwblq7zm.us-east-2.rds.amazonaws.com',  //'localhost',  //aa1v5oaurv6oyc5.cjhnbwblq7zm.us-east-2.rds.amazonaws.com  //  basebarraca.c87vonjdhyby.us-east-1.rds.amazonaws.com
+        user: 'BarracaBaseDatos',  //'root',BarracaCardona  // baseBarraca
+        password: 'BarracaBaseDatos', //'',  // BarracaCardona  //  baseBarraca
+        database:  'barracadb' //ebdb // 'barracadb'   //  'ng_prueba_db  //  baseBarraca
+       // port:'3306'
     }
 }
--- Server/build/keys.js
+++ Server/build/keys.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'barracadb' //ebdb // 'barracadb'   //  'ng_prueba_db
+        host: 'barracabasedatos.cjhnbwblq7zm.us-east-2.rds.amazonaws.com',
+        user: 'BarracaBaseDatos',
+        password: 'BarracaBaseDatos',
+        database: 'barracadb' //ebdb // 'barracadb'   //  'ng_prueba_db  //  baseBarraca
+        // port:'3306'
     }
 };
