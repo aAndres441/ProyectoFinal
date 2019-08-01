c8d437584d5e9c3fe57437e5b241f9f949e029dc
aAndres441 <46266307+aAndres441@users.noreply.github.com>
2019-08-01 09:01:08 -0300

    agrega buy

---
+++ Frontend/src/app/pages/buy/components/index.ts
@@ -0,0 +1,3 @@
+export * from './buy-detail/buy-detail.component';
+export * from './buy-list/buy-list.component';
+export * from './buy-new/buy-new.component';
---
+++ Frontend/src/app/pages/buy/container/index.ts
@@ -0,0 +1,1 @@
+export * from './buy/buy.component';
